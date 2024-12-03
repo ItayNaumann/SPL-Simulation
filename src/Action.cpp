2b398fc33bbfe297195842c83164ff1b39afc3d5
itaynau <itay.naumann@gmail.com>
2024-12-04 00:37:44 +0200

    numOfSteps is const so for is needed + formating

--- src/Action.cpp
+++ src/Action.cpp
@@ -1,21 +1,24 @@
 #include <Action.h>
 
-//SimulateStep
-SimulateStep::SimulateStep(const int numOfSteps):numOfSteps(numOfSteps),{
-
+// SimulateStep
+SimulateStep::SimulateStep(const int numOfSteps) : numOfSteps(numOfSteps)
+{
 }
 
-void SimulateStep::act(Simulation &simulation){
-    while (numOfSteps > 0){
-        for (Plan &plan : simulation.getPlans()){
+void SimulateStep::act(Simulation &simulation)
+{
+    for (int i = 0; i < numOfSteps; i++)
+    {
+        for (Plan &plan : simulation.getPlans())
+        {
             plan.step();
         }
     }
-    
 }
-const string SimulateStep::toString() const{
+const string SimulateStep::toString() const
+{
     // Do stuff
 }
-SimulateStep *SimulateStep::clone() const{
-
+SimulateStep *SimulateStep::clone() const
+{
 }
